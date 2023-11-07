b4aa218b0f1c7cf5e32ae02d5fff94f0eece911c
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-07 10:56:57 -0300

    Add files via upload

---
+++ ScrollView/ScrollView/ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewApp.swift
+//  ScrollView
+//
+//  Created by Lucas Neves dos santos pompeu on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
